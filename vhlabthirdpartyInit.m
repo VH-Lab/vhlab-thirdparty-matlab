4c5338a26e35afe4041fcea02eda27dce33e49df
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-07-02 08:30:16 -0400

    added json JAVA code

---
+++ vhlabthirdpartyInit.m
@@ -0,0 +1,30 @@
+function vhlabthirdpartyInit
+% VHLABTHIRDPARTYINIT - add paths for vhlab-thirdparty-matlab toolbox
+% 
+% This toolbox is a collection of third-party open source software for use with the
+% VHLAB toolbox.
+%
+% See: http://code.vhlab.org
+% 
+
+mypath = fileparts(which('vhlabthirdpartyInit'));
+
+ % remove any paths that have the string 'vhlab-thirdparty-matlab' so we don't have stale
+% paths confusing anyone
+
+pathsnow = path;
+pathsnow_cell = split(pathsnow,pathsep);
+matches = contains(pathsnow_cell, 'vhlab-thirdparty-matlab');
+pathstoremove = char(strjoin(pathsnow_cell(matches),pathsep));
+rmpath(pathstoremove);
+
+  % add everything except '.git' directories
+pathstoadd = genpath(mypath);
+pathstoadd_cell = split(pathstoadd,pathsep);
+matches=(~contains(pathstoadd_cell,'.git'));
+pathstoadd = char(strjoin(pathstoadd_cell(matches),pathsep));
+addpath(pathstoadd);
+
+javaaddpath([mypath filesep 'java']);
+
+
